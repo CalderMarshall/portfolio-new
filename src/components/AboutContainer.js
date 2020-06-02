8bfa26c92593b3468aaa8029f680f791ee7e304e
CalderMarshall <cmarshall72@gmail.com>
2020-06-02 07:55:07 -0400

    adding some van info

--- src/components/AboutContainer.js
+++ src/components/AboutContainer.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
-import pdf from '../static/docs/CalderMarshallResume.pdf';
-import ATL from '../static/images/atlanta-united-fc.svg';
-import OUT from '../static/images/outkast.png';
-import SEAL from '../static/images/seal.png';
-import BRAVES from '../static/images/braves.png';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+import pdf from "../static/docs/CalderMarshallResume.pdf";
+import ATL from "../static/images/atlanta-united-fc.svg";
+import OUT from "../static/images/outkast.png";
+import SEAL from "../static/images/seal.png";
+import BRAVES from "../static/images/braves.png";
 
 export default function AboutContainer() {
   return (
-    <Container style={{ padding: '0 5px'}} maxWidth={false}>
+    <Container style={{ padding: "0 5px" }} maxWidth={false}>
       <div
         style={{
-          backgroundColor: '#ededed',
+          backgroundColor: "#ededed",
           borderRadius: 10,
           padding: 15,
         }}
@@ -22,16 +22,16 @@ export default function AboutContainer() {
           About Me
         </Typography>
         <Typography paragraph variant="subtitle1">
-          Hey, I'm Calder, a User Interface Engineer for{' '}
+          Hey, I'm Calder, a User Interface Engineer for{" "}
           <Link target="none" href="https://about.sharecare.com/">
             Sharecare
-          </Link>{' '}
+          </Link>{" "}
           in Atlanta.
         </Typography>
         <Typography paragraph>
           I spend most of my time at Sharecare building functional React
           components for our web platform. You can view the web platform after
-          creating an{' '}
+          creating an{" "}
           <Link target="none" href="https://you.sharecare.com/">
             account
           </Link>
@@ -42,7 +42,7 @@ export default function AboutContainer() {
           I also spend a fair amount of time unit testing these components to
           ensure they will work properly. In addition, towards the end of 2019,
           I played a large part in updating certain sections of the Sharecare
-          Web Application to meet{' '}
+          Web Application to meet{" "}
           <Link
             target="none"
             href="https://www.w3.org/WAI/standards-guidelines/wcag/"
@@ -52,20 +52,20 @@ export default function AboutContainer() {
           .
         </Typography>
         <Typography paragraph>
-          Begrudgingly, this also includes making these views compliant in
-          older, less modern browsers. My work at Sharecare also includes
-          working with NodeJS servers when necessary.
+          This also includes making these views compliant in older, less modern
+          browsers. My work at Sharecare also includes working with and
+          debugging NodeJS servers when necessary.
         </Typography>
         <Typography paragraph>
-          My programming career started at{' '}
+          My programming career started at{" "}
           <Link target="none" href="https://www.digitalcrafts.com/">
             Digital Crafts
-          </Link>{' '}
+          </Link>{" "}
           in Buckhead, February of 2017.
         </Typography>
         <Typography paragraph>
           Want to learn more about my qualifications and experience? Take a look
-          at my{' '}
+          at my{" "}
           <Link target="none" href={pdf}>
             resume
           </Link>
@@ -74,47 +74,54 @@ export default function AboutContainer() {
         <Typography paragraph>
           I am truly a problem solver at heart, which lends aptitude towards my
           skills as a developer. Outside of work, I spend a large amount of time
-          maintaining, repairing, riding, and modifying, my mountain bikes, motorcyle, and car.
+          maintaining, repairing, riding, and modifying, my mountain bikes,
+          motorcyle, and car.
+        </Typography>
+        <Typography paragraph>
+          That sweet 1987 Chevrolet G20 Van is my current outside of work
+          project. My girlfriend Allyson and I were lucky enough to buy this van
+          in Savannah for $330. We're really looking forward to getting it on
+          the road and going on some awesome mountain biking trips!
         </Typography>
       </div>
       <div
         style={{
-          backgroundColor: '#ededed',
+          backgroundColor: "#ededed",
           borderRadius: 10,
           padding: 15,
-          margin: '20px 0px',
+          margin: "20px 0px",
         }}
       >
         <img
-          style={{ margin: '0px 5px' }}
+          style={{ margin: "0px 5px" }}
           src={SEAL}
           alt="City Of Atlanta Seal"
           height="50"
           width="50"
         />
         <img
-          style={{ margin: '0px 5px' }}
+          style={{ margin: "0px 5px" }}
           src={ATL}
           alt="Atlanta United"
           height="50"
           width="50"
         />
         <img
-          style={{ margin: '0px 5px' }}
+          style={{ margin: "0px 5px" }}
           src={OUT}
           alt="Outkast"
           height="50"
           width="74"
         />
         <img
-          style={{ margin: '0px 5px' }}
+          style={{ margin: "0px 5px" }}
           src={BRAVES}
           alt="Atlanta Braves"
           height="50"
           width="50"
         />
 
-        <Typography style={{ padding: '10px 0px' }} variant="h6">
+        <Typography style={{ padding: "10px 0px" }} variant="h6">
           Atlanta, GA
         </Typography>
         <Typography paragraph>
--- src/components/ImagesGrid.js
+++ src/components/ImagesGrid.js
@@ -1,69 +1,84 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import GridItem from './GridItem';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import GridItem from "./GridItem";
+import Container from "@material-ui/core/Container";
 
 export default function ImagesGrid() {
   return (
-    <Container style={{padding: '0 5px'}}>
+    <Container style={{ padding: "0 5px" }}>
       <div
         style={{
-          backgroundColor: '#ededed',
+          backgroundColor: "#ededed",
           borderRadius: 10,
           padding: 15,
         }}
       >
-      <Typography variant="h6" paragraph align="center">
-        Here's how I spend my free time!
-      </Typography>
-      <Grid container spacing={3}>
-        <GridItem
-          imageUrl={require('../static/images/beer_run.jpg')}
-          title={'Talladega National Forest Skyway'}
-          subtitle={'Eastern Alabama'}
-        />
-        <GridItem
-          imageUrl={require('../static/images/cwmftf.jpg')}
-          title={'Coldwater Mountain Fat Tire Festival'}
-          subtitle={'Anniston, AL'}
-        />
-        <GridItem
-          imageUrl={require('../static/images/santa_cruz.jpg')}
-          title={'Bull Mountain'}
-          subtitle={'Dahlonega, GA'}
-        />
-        <GridItem
-          imageUrl={require('../static/images/harley.jpg')}
-          title={'Fort Mountain'}
-          subtitle={'GA-52, Chatsworth, GA'}
-        />
-        <GridItem
-          imageUrl={require('../static/images/vail.jpg')}
-          title={'Vail Ski Resort'}
-          subtitle={'Vail, CO'}
-        />
-        <GridItem
-          imageUrl={require('../static/images/bmw.jpg')}
-          title={'My E61'}
-          subtitle={'Atlanta, GA'}
-        />
-        <GridItem
-          imageUrl={require('../static/images/memphis.jpg')}
-          title={'Beale Street'}
-          subtitle={'Memphis, TN'}
-        />
-        <GridItem
-          imageUrl={require('../static/images/kitchen.jpg')}
-          title={'Pico de Gallo'}
-          subtitle={'Nashville, TN'}
-        />
-        <GridItem
-          imageUrl={require('../static/images/red_rocks.jpg')}
-          title={'Red Rocks Amphitheatre'}
-          subtitle={'Morrison, CO'}
-        />
-      </Grid>
+        <Typography variant="h6" paragraph align="center">
+          Here's how I spend my free time!
+        </Typography>
+        <Grid container spacing={3}>
+          <GridItem
+            imageUrl={require("../static/images/beer_run.jpg")}
+            title={"Talladega National Forest Skyway"}
+            subtitle={"Eastern Alabama"}
+          />
+          <GridItem
+            imageUrl={require("../static/images/cwmftf.jpg")}
+            title={"Coldwater Mountain Fat Tire Festival"}
+            subtitle={"Anniston, AL"}
+          />
+          <GridItem
+            imageUrl={require("../static/images/santa_cruz.jpg")}
+            title={"Bull Mountain"}
+            subtitle={"Dahlonega, GA"}
+          />
+          <GridItem
+            imageUrl={require("../static/images/harley.jpg")}
+            title={"Fort Mountain"}
+            subtitle={"GA-52, Chatsworth, GA"}
+          />
+          <GridItem
+            imageUrl={require("../static/images/vail.jpg")}
+            title={"Vail Ski Resort"}
+            subtitle={"Vail, CO"}
+          />
+          <GridItem
+            imageUrl={require("../static/images/bmw.jpg")}
+            title={"My E61"}
+            subtitle={"Atlanta, GA"}
+          />
+          <GridItem
+            imageUrl={require("../static/images/memphis.jpg")}
+            title={"Beale Street"}
+            subtitle={"Memphis, TN"}
+          />
+          <GridItem
+            imageUrl={require("../static/images/kitchen.jpg")}
+            title={"Pico de Gallo"}
+            subtitle={"Nashville, TN"}
+          />
+          <GridItem
+            imageUrl={require("../static/images/red_rocks.jpg")}
+            title={"Red Rocks Amphitheatre"}
+            subtitle={"Morrison, CO"}
+          />
+          <GridItem
+            imageUrl={require("../static/images/pisgah.jpg")}
+            title={"Looking Glass Falls"}
+            subtitle={"Brevard, NC"}
+          />
+          <GridItem
+            imageUrl={require("../static/images/van_2.jpg")}
+            title={"1987 Chevy G20"}
+            subtitle={"Atlanta, GA"}
+          />
+          <GridItem
+            imageUrl={require("../static/images/van_3.jpg")}
+            title={"1987 Chevy G20"}
+            subtitle={"Atlanta, GA"}
+          />
+        </Grid>
       </div>
     </Container>
   );
